fd1087722b263482b565d817ec96483742dc8841
Nuno Guerreiro <nunomguerreiro@gmail.com>
2016-03-12 17:50:49 +0000

    Python

---
+++ python/selenium.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+import time
+
+countries = ['Canada', 'Germany', 'Iceland', 'Pakistan', 'Singapore', 'South Africa']
+
+browser = webdriver.Chrome() # Get local session of firefox
+browser.get("http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk") # Load page
+
+for name in countries:
+
+	elem = browser.find_element_by_id("countryName") # Find the query box
+	elem.clear()
+	elem.send_keys(name + Keys.RETURN)
+	time.sleep(0.2) # Let the page load, will be added to the API
+
+	code = browser.find_element_by_id("countryCode")
+	print name,"\t\t" ,code.text;
+browser.close()
---
+++ python/squares.py
@@ -0,0 +1,18 @@
+def squares():
+  for x in xrange(1, 100 + 1):
+    print (x*x)
+		
+squares()
+print
+
+def primes():
+  print ("1")
+  for num in range(1, 50 + 1):
+		for div in range(2,num):
+			if (num % div) == 0:
+				print(num)								
+				break
+		else:
+			print("PRIME")
+
+primes()
